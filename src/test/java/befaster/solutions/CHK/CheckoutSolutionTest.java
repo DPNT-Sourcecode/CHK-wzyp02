3bc5463d4a0f6ac6b722a84b0de6050b71ee6574
sbx_user1051 <sbx_user1051@169.254.191.165>
2022-02-27 19:39:36 +0000

    Sun Feb 27 19:39:36 UTC 2022

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,2 +1,25 @@
-package befaster.solutions.CHK;public class CheckoutSolutionTest {
+package befaster.solutions.CHK;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckoutSolutionTest {
+	private CheckoutSolution checkoutSolution;
+
+	@Before
+	public void setup() {
+		checkoutSolution = new CheckoutSolution();
+	}
+
+	@Test
+	public void testInvalidInput() {
+		Assert.assertEquals(Integer.valueOf(-1), checkoutSolution.checkout("E"));
+	}
+
+	@Test
+	public void testCheckout() {
+		Assert.assertEquals(Integer.valueOf(180 + 45 + 15), checkoutSolution.checkout("AABBAAD"));
+	}
 }
+
