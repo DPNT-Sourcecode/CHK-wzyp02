3a16a33e9b7c42857b3a04094d140a2b0c6ccead
sbx_user1051 <sbx_user1051@169.254.191.165>
2022-02-27 19:51:00 +0000

    Sun Feb 27 19:51:00 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,22 +1,13 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
-
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        Set<Character> skusKey = new HashSet<>();
-        skusKey.add('A');
-        skusKey.add('B');
-        skusKey.add('C');
-        skusKey.add('D');
         Map<Character, Integer> counts = new HashMap<>();
         for(char c : skus.toCharArray()) {
-            if(skusKey.contains(c)) {
+            if(c >= 'A' && c <= 'D') {
                 int count = counts.getOrDefault(c, 0);
                 counts.put(c, count + 1);
             } else {
@@ -24,7 +15,20 @@ public class CheckoutSolution {
             }
         }
         int res = 0;
-        for()
+        for(char sku : counts.keySet()) {
+            int count = counts.get(sku);
+            if(sku == 'A') {
+                res += (count / 3) * 130 + (count % 3) * 50;
+            } else if(sku == 'B') {
+                res += (count / 2) * 45 + (count % 2) * 30;
+            } else if(sku == 'C') {
+                res += 20 * count;
+            } else {
+                res += 15 * count;
+            }
+        }
+        return res;
     }
 }
+
 
