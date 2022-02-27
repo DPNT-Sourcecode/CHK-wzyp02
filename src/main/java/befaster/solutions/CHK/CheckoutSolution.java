7d21823af86679ff0107a7427c9428ccf77e9076
sbx_user1051 <sbx_user1051@169.254.191.165>
2022-02-27 19:42:36 +0000

    Sun Feb 27 19:42:36 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,9 +2,14 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        Set<String> skusKey = new HashSet<>();
         
     }
 }
+
 
