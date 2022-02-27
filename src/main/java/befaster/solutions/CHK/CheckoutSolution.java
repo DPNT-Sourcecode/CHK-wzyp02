4028459f9617a73c7922d0e897fddecd208e508d
sbx_user1051 <sbx_user1051@169.254.191.165>
2022-02-27 19:48:36 +0000

    Sun Feb 27 19:48:36 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,16 +2,29 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        Set<String> skusKey = new HashSet<>();
-        skusKey.add("A");
-        skusKey.add("B");
-        skusKey.add("C");
-        skusKey.add("D");
-        
+        Set<Character> skusKey = new HashSet<>();
+        skusKey.add('A');
+        skusKey.add('B');
+        skusKey.add('C');
+        skusKey.add('D');
+        Map<Character, Integer> counts = new HashMap<>();
+        for(char c : skus.toCharArray()) {
+            if(skusKey.contains(c)) {
+                int count = counts.getOrDefault(c, 0);
+                counts.put(c, count + 1);
+            } else {
+                return -1;
+            }
+        }
+        int res = 0;
+        for()
     }
 }
+
